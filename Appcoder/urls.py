c2b749fe0c52284d61d0eb2e74e57d97569a358a
Rafael Roman <rafaelroman01@gmail.com>
2022-11-23 21:38:35 -0300

    cambios en el proyecto Template y Herencias de template urls dinamicas en los template

--- Appcoder/urls.py
+++ Appcoder/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from Appcoder.views import *
 
 urlpatterns = [
-    path("", inicio),
-    path("participantes/", participantes),
-    path("recreadores/", recreadores),
-    path("excursiones/", excursiones),
-    path("documentacion/", documentacion)
+    path("inicio/", inicio, name="coder-inicio"),
+    path("participantes/", participantes, name="coder-participantes"),
+    path("recreadores/", recreadores, name="coder-recreadores"),
+    path("excursiones/", excursiones, name="coder-excursiones"),
+    path("documentacion/", documentacion, name="coder-documentacion")
     
 ]
--- viajeescolar/urls.py
+++ viajeescolar/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("coder/", include("Appcoder.urls"))
+    path('coder/', include("Appcoder.urls"))
      
 ]
